21e5435453a10a53c9c68ae4f4297e61f5cb53d2
AleksaRatkovic <123995009+AleksaRatkovic@users.noreply.github.com>
2023-02-23 01:22:14 +0100

    Update Razlomak.cpp

--- Kompozicija/Razlomak.cpp
+++ Kompozicija/Razlomak.cpp
@@ -2,22 +2,7 @@
 
 Razlomak operator+(const Razlomak& r1, const Razlomak& r2)
 {
-    //my solution
-    /*if (r1.imenilac > r2.imenilac) {
-        if (r1.imenilac % r2.imenilac== 0) {
-            return Razlomak(r1.brojilac + (r1.imenilac / r2.imenilac) * r2.brojilac, r1.imenilac);
-        }
-        return Razlomak(r1.imenilac * r2.brojilac + r1.brojilac * r2.imenilac, r1.imenilac * r2.imenilac);
-    }
-    else if (r2.imenilac % r1.imenilac == 0) {
-        return Razlomak(r2.brojilac + (r2.imenilac / r1.imenilac) * r1.brojilac, r2.imenilac);
-    }
-    return Razlomak(r1.imenilac * r2.brojilac + r1.brojilac * r2.imenilac, r1.imenilac * r2.imenilac);*/
-
-    // Given solution
-
    return Razlomak(r1.brojilac * r2.imenilac + r1.imenilac * r2.brojilac, r1.imenilac * r2.imenilac);
-
 }
 
 bool operator>(const Razlomak& r1, const Razlomak& r2)
